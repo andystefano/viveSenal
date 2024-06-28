ab31c1c92bd384b5f22e12b0e901d3fe4af39735
ANDY Hormazabal <andy@andy.cl>
2024-06-28 00:34:51 -0400

    solved dr combu

--- src/componentes/ViveFooter.js
+++ src/componentes/ViveFooter.js
@@ -8,7 +8,7 @@ function ViveFooter({ minHeight }) {
   return (
     <footer className="bg-cream py-4 sd:py-10 md:py-10 lg:py-10 xl:py-10 px-4 sd:px-10 md:px-10 lg:px-10 xl:px-10">
       <div
-        className="grid grid-cols-3 sd:grid-cols-6 sm:grid-cols-6 lg:grid-cols-6 xl:grid-cols-6 grid-rows-2 gap-1"
+        className="grid grid-cols-2 sd:grid-cols-6 sm:grid-cols-6 lg:grid-cols-6 xl:grid-cols-6 grid-rows-2 gap-1"
         style={{ gridTemplateRows: "auto 2fr" }}
       >
         <div className="col-span-1">
@@ -31,12 +31,12 @@ function ViveFooter({ minHeight }) {
             </div>
           </div>
         </div>
-        <div className="col-span-1">
+        <div className="col-span-1  hidden sd:block md:block lg:block xl:block ">
           <div className="flex items-start">
             <span className="text-pink-400 ">
               <MdTripOrigin  className="text-base"   />
             </span>
-            <div className="text-black  font-libre-franklin   ml-1 sd:ml-2 md:ml-2 lg:ml-2 xl:ml-2  text-xs sd:font-sd md:font-sd lg:font-sd xl:font-sd text-left">
+            <div className="text-black font-libre-franklin   ml-1 sd:ml-2 md:ml-2 lg:ml-2 xl:ml-2  text-xs sd:font-sd md:font-sd lg:font-sd xl:font-sd text-left">
               Colaboran
             </div>
           </div>
@@ -47,17 +47,37 @@ function ViveFooter({ minHeight }) {
         </div>
         <div className="col-span-1 row-span-1  hidden sd:block md:block lg:block xl:block ">
         </div>
+
+
         <div className="col-span-1">
           <a href="http://www.antenna.cl/?utm_source=Antenna&utm_medium=Antenna&utm_campaign=ViveSenal"><img alt="Fundación Antenna" src="images/logoAntennaBlack.png" /></a>
         </div>
         <div className="col-span-1">
           <a href="https://btgpactual.cl/?utm_source=Antenna&utm_medium=Antenna&utm_campaign=ViveSenal"><img alt="BGT Pactual" src="images/logoBGTPactualBlack.png" /></a>
         </div>
+
+        <div className="col-span-1  block sd:hidden md:hidden lg:hidden xl:hidden ">
+          <div className="flex items-start">
+            <span className="text-pink-400 ">
+              <MdTripOrigin  className="text-base"   />
+            </span>
+            <div className="text-black font-libre-franklin   ml-1 sd:ml-2 md:ml-2 lg:ml-2 xl:ml-2  text-xs sd:font-sd md:font-sd lg:font-sd xl:font-sd text-left">
+              Colaboran
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1  block sd:hidden md:hidden lg:hidden xl:hidden ">
+          <div className="flex items-start">
+           
+          </div>
+        </div>
+
         <div className="col-span-1">
           <a href="https://cervezaloa.cl/?utm_source=Antenna&utm_medium=Antenna&utm_campaign=ViveSenal"><img alt="LOA Cerveceria" src="images/logoLOABlack.png" /></a>
         </div>
         <div className="col-span-1  sd:block md:block lg:block xl:block ">
-          <a href="https://drkombu.cl/?utm_source=Antenna&utm_medium=Antenna&utm_campaign=ViveSenal"><img alt="Dr. Kombu" src="images/Dr_Kombu.png" /></a>
+          <a href="https://drkombu.cl/?utm_source=Antenna&utm_medium=Antenna&utm_campaign=ViveSenal"><img alt="Dr. Kombu" src="images/Dr_KombuB.png" /></a>
         </div>
         <div className="col-span-1 hidden sd:block md:block lg:block xl:block  ">
           <div className="flex items-center justify-center  mt-4">
