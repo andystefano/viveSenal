4352fbb2a7b2fbf17c40d849ef77f355aee53bee
Andy Hormazabal <andy@andy.cl>
2024-07-11 12:00:21 -0400

    update countdown data

--- src/componentes/CountDown.js
+++ src/componentes/CountDown.js
@@ -1,7 +1,7 @@
 function CountDown() {
 
     function countdown() {
-        const endDate = new Date("2024-07-10T20:00:00").getTime(); // Cambia esta fecha por la fecha deseada
+        const endDate = new Date("2024-08-08T20:00:00").getTime(); // Cambia esta fecha por la fecha deseada
         const now = new Date().getTime();
         const timeRemaining = endDate - now;
         console.log('timeRemaining:::',timeRemaining);
