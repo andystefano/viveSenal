1dbaf8a829b4e3e7fdf34c85373b690f7eb49cbb
ANDY Hormazabal <andy@andy.cl>
2024-07-13 01:07:32 -0400

    agosto a octubre

--- src/componentes/Section3.js
+++ src/componentes/Section3.js
@@ -35,7 +35,7 @@ function Section({ minHeight }) {
       PROGRA<span className="sd:hidden sm:hidden lg:hidden">-</span><br className="block sd:hidden sm:hidden lg:hidden" />MACIÓN
     </h1>
     <h1 className="text-3xl leading-none md:text-5xl xl:text-6xl text-white text-left font-unbounded  pl-7 sd:pl-14 md:pl-14 lg:pl-14 xl:pl-14 " style={{ color: 'rgb(0, 0, 0)' }}>
-      MAYO / JULIO
+      AGOSTO / OCTUBRE
     </h1>
   </div>
   <img style={{paddingTop:'3rem'}} src="images/cuposLimitados.png" className="hidden align-top w-14 mt-5 sd:mt-0 md:mt-0 lg:mt-0 xl:mt-0" alt="Cupos limitados" />
@@ -63,7 +63,7 @@ function Section({ minHeight }) {
             color: "#000",
           }}
         >
-          MAYO / JULIO
+          AGOSTO / OCTUBRE
         </h1>
       </div>
 
@@ -78,14 +78,20 @@ function Section({ minHeight }) {
             <div className="text-center w-full h-full px-3 sd:px-7 md:px-7 lg:px-7 py-3 sd:py-7 md:py-7 lg:py-7 xl:py-7">
               <div>
               <h1 className="text-normal sd:text-3xl md:text-3xl lg:text-3xl xl:text-3xl text-left font-bold font-unbounded ">
-                Edificio Alonso, Vitacura
+                Galería NAC, Vitacura
                 </h1>
                 <p className="text-xs sd:text-sm md:text-sm lg:text-sm xl:text-sm mt-0 sd:text-2xl md:text-2xl xl:text-2xl text-left font-libre-franklin sd:mt-4 md:mt-4 lg:mt-4">
-                Javier Toro Blum es un artista que trabaja principalmente con la luz y la oscuridad. Sus obras buscan desafiar nuestra percepción visual, generando diversas experiencias que cuestionan lo que podemos ver.
+                Arte digital, dibujos y mucho más en el corazón de Vitacura.
                 </p>
                 <p className="text-xs sd:text-sm md:text-sm lg:text-sm xl:text-sm mt-3 sd:text-2xl md:text-2xl xl:text-2xl text-left font-libre-franklin sd:mt-3 md:mt-3 lg:mt-3">
-Música a cargo del propio artista.
+                Comenzaremos visitando las exposiciones de Cristóbal Cea y Josefina Frederick, para después recorrer los casi 10 talleres que son parte de los artistas de NAC.
                 </p>
+
+                <p className="text-xs sd:text-sm md:text-sm lg:text-sm xl:text-sm mt-3 sd:text-2xl md:text-2xl xl:text-2xl text-left font-libre-franklin sd:mt-3 md:mt-3 lg:mt-3">
+                Música por confirmar.</p>
+
+                
+
               </div>
               <a href="#tickets"><button className="absolute bottom-0 left-0 mx-7 my-7 flex items-center justify-center px-6 py-1 text-black  sd:text-normal md:text-2xl lg:text-2xl xl:text-2xl  font-bold rounded-full border-2 border-black mt-4 font-unbounded hover:text-white hover:border-white">
                 Tickets &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -102,12 +108,12 @@ Música a cargo del propio artista.
             <div className="text-center w-full h-full px-3 sd:px-7 md:px-7 lg:px-7  py-3 sd:py-7 md:py-7 lg:py-7 xl:py-7">
               <div>
               <h1 className="text-normal sd:text-3xl md:text-3xl lg:text-3xl xl:text-3xl text-left font-bold font-unbounded ">
-              MNBA, Bellas Artes</h1>
+              Taller 99, Providencia</h1>
                 <p className="text-xs sd:text-sm md:text-sm lg:text-sm xl:text-sm mt-0 sd:text-2xl md:text-2xl xl:text-2xl text-left font-libre-franklin sd:mt-4 md:mt-4 lg:mt-4">
-                Únete a una especial visita nocturna al Museo Nacional de Bellas Artes (MNBA), donde conocerás "El día más hermoso", una exposición que destaca la obra de Carlos Leppe, reconocido como uno de los principales exponentes de la performance en Latinoamérica.
+                ¡Sé artista por una noche!
                 </p>
                 <p className="text-xs sd:text-sm md:text-sm lg:text-sm xl:text-sm mt-3 sd:text-2xl md:text-2xl xl:text-2xl text-left font-libre-franklin sd:mt-3 md:mt-3 lg:mt-3">
-                Música a cargo de Inad.
+                Conoce la historia del mítico Taller 99, un lugar en el que han participado grandes artistas desde 1956. Aprenderás sobre la técnica del grabado y cómo hacer tu propia obra de arte. 
                 </p>
               </div>
               <a href="#tickets"><button className="absolute bottom-0 left-0 mx-7 my-7 flex items-center justify-center px-6 py-1 text-black text-normal sd:text-normal md:text-2xl lg:text-2xl xl:text-2xl font-bold rounded-full border-2 border-black mt-4 font-unbounded hover:text-white hover:border-white">
@@ -125,14 +131,20 @@ Música a cargo del propio artista.
             <div className="text-center w-full h-full px-3 sd:px-7 md:px-7 lg:px-7 py-3 sd:py-7 md:py-7 lg:py-7 xl:py-7">
               <div>
                 <h1 className="text-normal sd:text-3xl md:text-3xl lg:text-3xl xl:text-3xl text-left font-bold font-unbounded ">
-                Centro NAVE, Barrio Yungay
+                Artistas del Talleres Origen, Lo Barnechea
                 </h1>
                 <p className="text-xs sd:text-sm md:text-sm lg:text-sm xl:text-sm mt-2 sd:text-2xl md:text-2xl xl:text-2xl text-left font-libre-franklin sd:mt-4 md:mt-4 lg:mt-4">
-                La artista, coreógrafa y bailarina Alexandra Mabes abre el ensayo de su obra "Cu", pieza interdisciplinaria inspirada en el cobre. Además, tendrás la oportunidad de conocer este alucinante proyecto de recuperación arquitectónica, realizado por el destacado arquitecto chileno Smiljan Radic.</p>
+                Descubre a siete artistas mujeres y conoce de cerca su arte.
+                </p>
                 
                 <p className="text-xs sd:text-sm md:text-sm lg:text-sm xl:text-sm mt-0 sd:text-2xl md:text-2xl xl:text-2xl text-left font-libre-franklin sd:mt-3 md:mt-3 lg:mt-3">
-                Performance a cargo de la propia artista.
+                Cerca de Plaza San Enrique y con una hermosa vista al río, un grupo de artistas dan vida a Talleres Origen. Además de visitar sus espacios creativos, podrás disfrutar de la naturaleza que rodea este lugar.
                 </p>
+
+                <p className="text-xs sd:text-sm md:text-sm lg:text-sm xl:text-sm mt-0 sd:text-2xl md:text-2xl xl:text-2xl text-left font-libre-franklin sd:mt-3 md:mt-3 lg:mt-3">
+                Música por confirmar.
+                </p>
+
               </div>
               <a href="#tickets"><button className="absolute bottom-0 left-0 mx-7 my-7 flex items-center justify-center px-6 py-1 text-black  sd:text-normal md:text-2xl lg:text-2xl xl:text-2xl  font-bold rounded-full border-2 border-black mt-4 font-unbounded hover:text-white hover:border-white">
                 Tickets &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
