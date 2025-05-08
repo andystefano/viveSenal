28484fc365bdee46d0b2d34cf42bf9d6539e7e36
Andy Hormazabal <andy@andy.cl>
2025-05-08 11:32:16 -0400

    fix colores programacion

--- src/componentes/Section3.js
+++ src/componentes/Section3.js
@@ -117,7 +117,7 @@ function Section({ minHeight }) {
         </div>
         <div className="relative p-0 sd:p-5 md:p-5 lg:p-5 xl:p-5">
           <img src="images/programacion/p2.png" alt="Imagen" className="w-full" />
-          <div className="m-0 sd:m-5 md:m-5 lg:m-5 xl:m-5 absolute inset-0 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 bgTGreen bg-opacity-100 text-black">
+          <div className="m-0 sd:m-5 md:m-5 lg:m-5 xl:m-5 absolute inset-0 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 bgTRed bg-opacity-100 text-black">
           <div className="text-center w-full h-full px-3 sd:px-7 md:px-7 lg:px-7  py-3 sd:py-7 md:py-7 lg:py-7 xl:py-7">
               <div>
               <h1 className="text-normal sd:text-3xl md:text-3xl lg:text-3xl xl:text-3xl text-left font-bold font-unbounded ">
@@ -141,7 +141,7 @@ function Section({ minHeight }) {
         </div>
         <div className="relative p-0 sd:p-5 md:p-5 lg:p-5 xl:p-5">
           <img src="images/programacion/p3.png" alt="Imagen" className="w-full" />
-          <div className="m-0 sd:m-5 md:m-5 lg:m-5 xl:m-5 absolute inset-0 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 bgTGreen bg-opacity-100 text-black">
+          <div className="m-0 sd:m-5 md:m-5 lg:m-5 xl:m-5 absolute inset-0 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 bgTPink bg-opacity-100 text-black">
           <div className="text-center w-full h-full px-3 sd:px-7 md:px-7 lg:px-7  py-3 sd:py-7 md:py-7 lg:py-7 xl:py-7">
           <div>
               <h1 className="text-normal sd:text-3xl md:text-3xl lg:text-3xl xl:text-3xl text-left font-bold font-unbounded ">
